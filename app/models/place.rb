2366af61cd741889d6071322ab9e8220599feb1a
Giovanni Flores <geflores5@gmailcom>
2017-03-06 05:41:30 +0000

    Create comment table

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,8 +8,11 @@ class Place < ApplicationRecord
   end
 
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, presence: true, length: { minimum: 3 }
   validates :description, presence: true
   validates :address, presence: true
